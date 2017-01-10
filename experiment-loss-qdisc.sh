3136b1308b0517fa8cc5283efe95ab45bff2faec
Venkat Arun <venkatarun95@gmail.com>
2017-01-10 14:26:52 +0530

    Auto indent.

--- experiment-loss-qdisc.sh
+++ experiment-loss-qdisc.sh
@@ -10,60 +10,60 @@ on_duration=60000 # (ms)
 rat_file=../evaluations/rats/fig2-linkspeed/bigbertha-100x.dna.5
 
 if [[ ! -d $output_directory ]]; then
-		mkdir $output_directory
+    mkdir $output_directory
 fi
 
 if [[ $1 == "run" ]]; then
-		for (( i = 0; i < 11; i=i+1 )); do
-				loss_rate=`awk -v i=$i 'END{if(i < 5)print i * 0.2; else print (i-4) * 1.0}' /dev/null`
-				echo "Running on loss rate = $loss_rate"
-				
-				for cc_type in "markovian" "cubic" "reno" "pcc"; do
+    for (( i = 0; i < 11; i=i+1 )); do
+	loss_rate=`awk -v i=$i 'END{if(i < 5)print i * 0.2; else print (i-4) * 1.0}' /dev/null`
+	echo "Running on loss rate = $loss_rate"
+	
+	for cc_type in "markovian" "cubic" "reno" "pcc"; do
             tcp_dir=$output_directory/$cc_type::$loss_rate
             if [[ -d $tcp_dir ]]; then
                 continue
             fi
-						runstr="sudo ./long-run-qdisc.sh run $cc_type $link_rate $min_delay $loss_rate $output_directory $nsrc:continuous $queue_length $on_duration $rat_file"
-						echo $runstr
-						$runstr
-						mv $output_directory/$cc_type $tcp_dir
-				done
-		done
+	    runstr="sudo ./long-run-qdisc.sh run $cc_type $link_rate $min_delay $loss_rate $output_directory $nsrc:continuous $queue_length $on_duration $rat_file"
+	    echo $runstr
+	    $runstr
+	    mv $output_directory/$cc_type $tcp_dir
+	done
+    done
 
 elif [[ $1 == "graph" ]]; then
-		if [[ -d $output_directory/graphdir ]]; then
-				trash $output_directory/graphdir
-		fi
-		mkdir $output_directory/graphdir
+    if [[ -d $output_directory/graphdir ]]; then
+	trash $output_directory/graphdir
+    fi
+    mkdir $output_directory/graphdir
 
-		for tcp_dir in $output_directory/*::*; do
-				tcp=`expr "$tcp_dir" : ".*/\([^/]*\)::[0-9.]*"`
-				loss_rate=`expr "$tcp_dir" : ".*::\([0-9.]*\)"`
-				
-				# Gather statistics
+    for tcp_dir in $output_directory/*::*; do
+	tcp=`expr "$tcp_dir" : ".*/\([^/]*\)::[0-9.]*"`
+	loss_rate=`expr "$tcp_dir" : ".*::\([0-9.]*\)"`
+	
+	# Gather statistics
         read-tcptrace-output $tcp_dir/$tcp.pcap-trace
-				echo $loss_rate $tpt >>$output_directory/graphdir/$tcp.dat
-		done
+	echo $loss_rate $tpt >>$output_directory/graphdir/$tcp.dat
+    done
 
-		# Create gnuplot script
-		gnuplot_script="set xlabel 'Loss %'; set ylabel 'Throughput (Mbps)';
+    # Create gnuplot script
+    gnuplot_script="set xlabel 'Loss %'; set ylabel 'Throughput (Mbps)';
     set terminal svg fsize 14; set output '$output_directory/graphdir/loss-tpt.svg'; 
     plot " >>$output_directory/graphdir/loss-tpt.gnuplot
-		for tcp in $output_directory/graphdir/*.dat; do
-				tcp_nice=`expr "$tcp" : ".*/\([^/]*\).dat"`
-				echo $tcp $tcp_nice
-				gnuplot_script="$gnuplot_script '$tcp' using 1:2 title '$tcp_nice' with lines, "
-		done
-		echo $gnuplot_script >$output_directory/graphdir/loss-tpt.gnuplot
-	
-		gnuplot -p $output_directory/graphdir/loss-tpt.gnuplot
-		inkscape --export-png=$output_directory/graphdir/loss-tpt.png -b '#ffffff' -D $output_directory/graphdir/loss-tpt.svg
-		display $output_directory/graphdir/loss-tpt.png
+    for tcp in $output_directory/graphdir/*.dat; do
+	tcp_nice=`expr "$tcp" : ".*/\([^/]*\).dat"`
+	echo $tcp $tcp_nice
+	gnuplot_script="$gnuplot_script '$tcp' using 1:2 title '$tcp_nice' with lines, "
+    done
+    echo $gnuplot_script >$output_directory/graphdir/loss-tpt.gnuplot
+    
+    gnuplot -p $output_directory/graphdir/loss-tpt.gnuplot
+    inkscape --export-png=$output_directory/graphdir/loss-tpt.png -b '#ffffff' -D $output_directory/graphdir/loss-tpt.svg
+    display $output_directory/graphdir/loss-tpt.png
 
 elif [[ $1 == "clean" ]]; then
-		trash $output_directory
+    trash $output_directory
 
 else
-		echo "Unrecognized command '$1'."
-		echo "   Expected one of [run|graph|clean]"
+    echo "Unrecognized command '$1'."
+    echo "   Expected one of [run|graph|clean]"
 fi
--- long-run-qdisc.sh
+++ long-run-qdisc.sh
@@ -10,33 +10,33 @@ off_duration=1 # in ms
 HZ=100 # HZ value of kernel
 
 output_file_name() {
-		if [[ $cc_type == "markovian" ]]; then
-				of_dir=$output_directory/markovian
-				of_name=$of_dir/markovian
-		elif [[ $cc_type == "remy" ]]; then
-				of_dir=$output_directory/remy # Not giving rat name as of now
-				of_name=$of_dir/remy
-		elif [[ $cc_type == "sprout" ]] || [ $cc_type == "pcc" ] || [[ $cc_type == "pcp" ]] || [[ $cc_type == "cubic" ]] || [[ $cc_type == "reno" ]]; then
-				of_dir=$output_directory/$cc_type
-				of_name=$of_dir/$cc_type
-		else
-				echo "Control shouldn't reach here"
-				exit
-		fi
+    if [[ $cc_type == "markovian" ]]; then
+	of_dir=$output_directory/markovian
+	of_name=$of_dir/markovian
+    elif [[ $cc_type == "remy" ]]; then
+	of_dir=$output_directory/remy # Not giving rat name as of now
+	of_name=$of_dir/remy
+    elif [[ $cc_type == "sprout" ]] || [ $cc_type == "pcc" ] || [[ $cc_type == "pcp" ]] || [[ $cc_type == "cubic" ]] || [[ $cc_type == "reno" ]]; then
+	of_dir=$output_directory/$cc_type
+	of_name=$of_dir/$cc_type
+    else
+	echo "Control shouldn't reach here"
+	exit
+    fi
 }
 
 if [[ $1 == "run" ]]; then
-		# Read arguments
-		cc_type=$2
-		link_rate=$3
-		min_delay=$4
-		loss_rate=$5
-		output_directory=$6
-		nsrc=`expr "$7" : '\(.*\):.*'`
-		traffic_type=`expr "$7" : '.*:\(.*\)'`
-		queue_length=$8
+    # Read arguments
+    cc_type=$2
+    link_rate=$3
+    min_delay=$4
+    loss_rate=$5
+    output_directory=$6
+    nsrc=`expr "$7" : '\(.*\):.*'`
+    traffic_type=`expr "$7" : '.*:\(.*\)'`
+    queue_length=$8
     run_time=$9
-		delta_conf=$10 # If present
+    delta_conf=${10} # If present
 
     # Set up appropriate qdiscs
     op_netem=add
@@ -50,79 +50,82 @@ if [[ $1 == "run" ]]; then
     sudo tc qdisc $op_tbf   dev lo parent 1:1 handle 10: tbf rate $(echo $link_rate)mbit limit $queue_length burst $queue_length
 
     # Setup output files
-		output_file_name # sets of_name and of_dir
-		if [[ ! -d $of_dir ]]; then
-				mkdir $of_dir
-		fi
+    output_file_name # sets of_name and of_dir
+    if [[ ! -d $of_dir ]]; then
+	mkdir $of_dir
+    fi
 
     if [[ -f /tmp/long-run-qdisc.pcap ]]; then
         rm /tmp/long-run-qdisc.pcap
     fi
     tcpdump -w /tmp/long-run-qdisc.pcap -i lo -n &
-		if [[ $cc_type == "markovian" ]]; then
-				delta_conf=$9
-				echo "Assuming receiver is available at $receiver_ip"
+    ping $receiver_ip > $of_name.ping &
+    if [[ $cc_type == "markovian" ]]; then
+	echo "Assuming receiver is available at $receiver_ip"
 
-				# export MIN_RTT=`awk -v min_delay=$min_delay 'END{print 2*min_delay;}' /dev/null`
-        export MIN_RTT=10000000
+	#export MIN_RTT=`awk -v min_delay=$min_delay 'END{print 2*min_delay;}' /dev/null`
+	export MIN_RTT=10000000
 
-				./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=markovian delta_conf=$delta_conf" $nsrc $traffic_type $run_time $on_duration $off_duration \
-						                      1> $of_name.stdout 2> $of_name.stderr
+	./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=markovian delta_conf=$delta_conf" $nsrc $traffic_type $run_time $on_duration $off_duration \
+				  1> $of_name.stdout 2> $of_name.stderr
 
-		elif [[ $cc_type == "remy" ]]; then
-				rat_file=$9
-				echo "Assuming receiver is available at $receiver_ip"
-				export MIN_RTT=1
-				./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=remy if=$rat_file" $nsrc $traffic_type $run_time $on_duration $off_duration \
-						                      1> $of_name.stdout 2> $of_name.stderr
+    elif [[ $cc_type == "remy" ]]; then
+	rat_file=$9
+	echo "Assuming receiver is available at $receiver_ip"
+	export MIN_RTT=1
+	./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=remy if=$rat_file" $nsrc $traffic_type $run_time $on_duration $off_duration \
+				  1> $of_name.stdout 2> $of_name.stderr
 
-		elif [[ $cc_type == "sprout" ]]; then
-				echo "Assuming sprout server is available at $receiver_ip"
-				$bin_dir/sproutbt2 $receiver_ip 60001 \
-						               1> $of_name.stdout 2> $of_name.stderr &
-				child_pid=$!
-				echo $child_pid
-				sleep $(( $run_time / 1000 + 35 ))
-				kill $child_pid
-				# Remove the part where the link was doing nothing, waiting for sprout precomputation
-				awk -F ' ' 'BEGIN {x=0} {if(x==1 || ($1 == "#" && $2 != "base"))print $0; else {if($2=="+"){print "# base timestamp:", $1;print $0; x=1;}}}' $of_name.uplink >$of_name.uplink2
-				mv $of_name.uplink2 $of_name.uplink
+    elif [[ $cc_type == "sprout" ]]; then
+	echo "Assuming sprout server is available at $receiver_ip"
+	$bin_dir/sproutbt2 $receiver_ip 60001 \
+			   1> $of_name.stdout 2> $of_name.stderr &
+	child_pid=$!
+	echo $child_pid
+	sleep $(( $run_time / 1000 + 35 ))
+	kill $child_pid
+	# Remove the part where the link was doing nothing, waiting for sprout precomputation
+	awk -F ' ' 'BEGIN {x=0} {if(x==1 || ($1 == "#" && $2 != "base"))print $0; else {if($2=="+"){print "# base timestamp:", $1;print $0; x=1;}}}' $of_name.uplink >$of_name.uplink2
+	mv $of_name.uplink2 $of_name.uplink
 
-		elif [[ $cc_type == "pcp" ]]; then
-				echo "Assuming 'pcp-server' was run at $receiver_ip:8745"
-				./run-pcc-sender.sh "$bin_dir/pcp $receiver_ip 8745 1000000000 1 0.00001 1" $nsrc $traffic_type $run_time $on_duration $off_duration
-				> $of_name.stdout 2> $of_name.stderr
+    elif [[ $cc_type == "pcp" ]]; then
+	echo "Assuming 'pcp-server' was run at $receiver_ip:8745"
+	./run-pcc-sender.sh "$bin_dir/pcp $receiver_ip 8745 1000000000 1 0.00001 1" $nsrc $traffic_type $run_time $on_duration $off_duration
+	> $of_name.stdout 2> $of_name.stderr
 
-		elif [[ $cc_type == "cubic" ]] || [[ $cc_type == "reno" ]]; then
-				echo "Assuming 'iperf -s' was run at $receiver_ip"
-				sudo sysctl -w net.ipv4.tcp_congestion_control=$cc_type
-				#echo "Using default kernel TCP as root priviledges are required to change TCP"
-				./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=kernel offduration=1 onduration=$run_time traffic_params=deterministic,num_cycles=1" $nsrc $traffic_type $run_time $on_duration $off_duration \
-						                      1> $of_name.stdout 2> $of_name.stderr
+    elif [[ $cc_type == "cubic" ]] || [[ $cc_type == "reno" ]]; then
+	echo "Assuming 'iperf -s' was run at $receiver_ip"
+	sudo sysctl -w net.ipv4.tcp_congestion_control=$cc_type
+	#echo "Using default kernel TCP as root priviledges are required to change TCP"
+	./run-genericcc-sender.sh "$bin_dir/sender sourceip=$source_ip serverip=$receiver_ip cctype=kernel offduration=1 onduration=$run_time traffic_params=deterministic,num_cycles=1" $nsrc $traffic_type $run_time $on_duration $off_duration \
+				  1> $of_name.stdout 2> $of_name.stderr
 
-		elif [[ $cc_type == "pcc" ]]; then
-				echo "Assuming pcc receiver was run at $receiver_ip"
-				./run-pcc-sender.sh "$bin_dir/appclient $receiver_ip 9000" $nsrc $traffic_type $run_time $on_duration $off_duration \
-						                1> $of_name.stdout 2> $of_name.stderr
-		else
-				echo "Could not find cc_type '$cc_type'. It is either unsupported or not yet implemented"
-		fi
+    elif [[ $cc_type == "pcc" ]]; then
+	echo "Assuming pcc receiver was run at $receiver_ip"
+	./run-pcc-sender.sh "$bin_dir/appclient $receiver_ip 9000" $nsrc $traffic_type $run_time $on_duration $off_duration \
+			    1> $of_name.stdout 2> $of_name.stderr
+    else
+	echo "Could not find cc_type '$cc_type'. It is either unsupported or not yet implemented"
+    fi
     pkill tcpdump
     pkill tcpdump
+    pkill ping
+    pkill ping
     tcptrace -lu /tmp/long-run-qdisc.pcap > $of_name.pcap-trace
     rm /tmp/long-run-qdisc.pcap
     
 elif [[ $1 == "graph" ]]; then
-		output_directory=$2
-		if [[ -d $output_directory/graphdir ]]; then
-				rm -r $output_directory/graphdir
-		fi
-		mkdir $output_directory/graphdir
-		for dir in $output_directory/*; do
-				if [[ ! -d $dir ]] || [[ $dir == *graphdir ]]; then
-						continue
-				fi
-				nice_name=`expr "$dir" : '.*/\([^/]*\)'`
+
+    output_directory=$2
+    if [[ -d $output_directory/graphdir ]]; then
+	rm -r $output_directory/graphdir
+    fi
+    mkdir $output_directory/graphdir
+    for dir in $output_directory/*; do
+	if [[ ! -d $dir ]] || [[ $dir == *graphdir ]]; then
+	    continue
+	fi
+	nice_name=`expr "$dir" : '.*/\([^/]*\)'`
 
         if [[ `grep throughput $dir/$nice_name.pcap-trace | wc -l` -ge 2 ]]; then
             echo "Error: multiple connections found in trace. Skipping"
@@ -131,24 +134,24 @@ elif [[ $1 == "graph" ]]; then
 
         tpt=`grep throughput $dir/$nice_name.pcap-trace | awk '{if ($2 > $5) print $2*1e-6; else print $5*1e-6}'`
         echo $nice_name $tpt >> $output_directory/graphdir/tpt.data
-				#echo $nice_name `grep throughput $dir/$nice_name.stats | awk -F ' ' '{print $3}'` `grep queueing $dir/$nice_name.stats | awk -F ' ' '{print $6}'` >> $output_directory/graphdir/tpt-del.data
-		done
+	#echo $nice_name `grep throughput $dir/$nice_name.stats | awk -F ' ' '{print $3}'` `grep queueing $dir/$nice_name.stats | awk -F ' ' '{print $6}'` >> $output_directory/graphdir/tpt-del.data
+    done
 
 elif [[ $1 == "clean" ]]; then
-		echo "Not yet implemented"
+    echo "Not yet implemented"
 else
-		echo "Usage: long-run command [args]"
-		echo "  Commands:"
-		echo "    run - Run emulation. Usage: 'run cc_type link_rate min_delay loss_rate output_directory nsrc:exponential|continuous queue_length run_time [delta_conf|rat_file]'"
+    echo "Usage: long-run command [args]"
+    echo "  Commands:"
+    echo "    run - Run emulation. Usage: 'run cc_type link_rate min_delay loss_rate output_directory nsrc:exponential|continuous queue_length run_time [delta_conf|rat_file]'"
     echo "    graph - Graph results from directory. Usage: 'graph output_directory'"
     echo "    clean - Clean directory. Usage: 'clean output_directory'"
-		echo "  Explanation:"
-		echo "    cc_type: One of markovian, remy, sprout, pcc, pcp, cubic and reno."
-		echo "    link_rate: Rate for both up- and down-link in Mbits/s."
-		echo "    min_delay: Minimum delay in ms."
-		echo "    loss_rate: % packet loss."
-		echo "    output_directory: Directory where both raw data and graphs are dumped."
-		echo "    nsrc: Number of senders + type of traffic. If exponential, predetermined exponential distribution will be used, else a predetermined continuous run will be used."
-		echo "    queue_length: Droptail queue length in bytes. If 0, infinite queue will be used."
+    echo "  Explanation:"
+    echo "    cc_type: One of markovian, remy, sprout, pcc, pcp, cubic and reno."
+    echo "    link_rate: Rate for both up- and down-link in Mbits/s."
+    echo "    min_delay: Minimum delay in ms."
+    echo "    loss_rate: % packet loss."
+    echo "    output_directory: Directory where both raw data and graphs are dumped."
+    echo "    nsrc: Number of senders + type of traffic. If exponential, predetermined exponential distribution will be used, else a predetermined continuous run will be used."
+    echo "    queue_length: Droptail queue length in bytes. If 0, infinite queue will be used."
     echo "    run_time: Time (in ms) to run the sender."
 fi
